abc578e0fc8d90e7267c3edafbd3de48a332d8f4
Efşan Albayrak <83309686+EfsanAlbayrak@users.noreply.github.com>
2022-11-05 12:27:25 +0300

    Update Main.java

--- vucutKitleIndexi/src/Main.java
+++ vucutKitleIndexi/src/Main.java
@@ -7,15 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Gerekli değişkenlerin tanımlanması
         double boy, kilo, vucutKitleIndexi;
+        
         Scanner input = new Scanner(System.in);
 
+        //kullanıcıdan boy ve kilo değerlerinin alınması
         System.out.println("Lütfen boyunuzu giriniz: ");
         boy = input.nextDouble();
 
         System.out.println("Lütfen kilonuzu giriniz: ");
         kilo = input.nextDouble();
 
+        //vücut kitle indexinin hesaplanması
         vucutKitleIndexi = kilo/ (boy*boy);
 
         System.out.println("Vücüt Kitle İndexiniz: " + vucutKitleIndexi);
